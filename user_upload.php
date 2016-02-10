374de0d79b05e808248e45cc7c9c2ab4d1f583aa
Jonas Bunawan <jonas_bun@yahoo.com>
2016-02-10 23:04:33 +1100

    dry_run enhancement
    Parsing data on csv file to multidimensional array
    Displaying invalid e-mail address based on FILTER_VALIDATE_EMAIL php built-in function
    Store data on multidimensional array to database only when the application is not run on dry run mode (Other functions are still executed)

--- user_upload.php
+++ user_upload.php
@@ -1,10 +1,13 @@
 <?php
-//error_reporting(0);
+// Statement to hide unnecessary MySQL Error Message. If System Error Message is preferred to be displayed, simply comment out the statement by placing // at the front of statement just like this comment line.
+error_reporting(0);
 
+	// Command Line Directives in short form
 	$shortcmdirective = "u:";
 	$shortcmdirective .= "p:";
 	$shortcmdirective .= "h:";
 	
+	// Command Line Directives in complete form
 	$longcmdirective = array(
 		"file:",
 		"create_table",
@@ -12,15 +15,21 @@
 		"help",
 	);
 	
+	// Declaring $count variable to count number of rows processed, $user variable to store users' data onto array, & $rows variable to count number of affected rows
 	$count = 0;
+	$rows = 0;
+	$user = array(array());
 	
+	// Storing Value of Command Line Directives by using getopt() function for both short and complete options.
 	$options = getopt($shortcmdirective, $longcmdirective);
 	
+	// First if conditional statement contains function to display help message when --help is called within application execution.
 	if(isset($options['help'])) {
+		echo "\n" . "Please find below for details of available command line directives that are built within this application (case sensitive)" . "\n"; 
 		echo "\n" . "-u : MySQL username" . "\n";
 		echo "-p : MySQL password" . "\n";
 		echo "-h : MySQL host" . "\n";
-		echo "--file [csv file name] : this is the name of the CSV to be parsed" . "\n";
+		echo "--file csvfilename : this is the name of the CSV to be parsed" . "\n";
 		echo "--create_table : this will cause the MySQL users table to be built (and no further action will be taken)" . "\n";
 		echo "--dry_run : this will be used with the --file directive in the instance that we want to run the script but not insert into the DB. All other functions will be executed, but the database won't be altered." . "\n";
 		echo "--help : which will output the above list of directives with details." . "\n";
@@ -51,6 +60,7 @@
 			}
 			
 			if(isset($options['create_table'])) {
+				$result = mysqli_query($connection, "SHOW TABLES LIKE 'USERS'");
 				
 				$SQL = "CREATE TABLE IF NOT EXISTS Users (
 						name VARCHAR(30) NOT NULL,
@@ -58,10 +68,15 @@
 						email	VARCHAR(50) UNIQUE
 						)";
 				
+				if(mysqli_num_rows($result) > 0) {
+					mysqli_query($connection, "DROP TABLE Users");
+					echo "\n" . "Users table exists and will be rebuilt shortly" . "\n";
+				}
+				
 				if($connection->query($SQL) == TRUE) {
 					echo "\n" . "Table Users was created successfully" . "\n";
 				} else {
-					echo "\n" . "Error creating table: " . $connection->error . "\n";
+					die( "\n" . "Error creating table: " . $connection->error . "\n");
 				}
 			}
 
@@ -71,40 +86,54 @@
 					$fw = fopen("php://stdout", "w");
 					$result = mysqli_query($connection, "SHOW TABLES LIKE 'Users'");
 					
-					if(!isset($options['dry_run'])){
-						if (mysqli_num_rows($result) > 0) {
-							fgetcsv($fp);
-							while($data = fgetcsv($fp,1000,",")){
-								if($data[0] and (!filter_var($data[2], FILTER_VALIDATE_EMAIL)) === false ) {
+					// Skipping csv file Header. Simply comment out below fgetcsv() function if the imported file does not have any header.
+					fgetcsv($fp);
+					
+					while($data = fgetcsv($fp,1000,",")){
+						if($data[0] and !filter_var($data[2], FILTER_VALIDATE_EMAIL) === false) {
 
-									$connection->query("INSERT INTO Users (name, surname, email) VALUES
+							$user[$count]['name'] = trim(ucfirst(strtolower($data[0])));
+							$user[$count]['surname'] = trim(ucfirst(strtolower($data[1])));
+							$user[$count]['e-mail address'] = trim(ucfirst(strtolower($data[2])));
+							
+							$count ++;
+						} elseif (!filter_var($data[2], FILTER_VALIDATE_EMAIL) === true){
+							fprintf($fw, "\n" . $data[2] . " is in invalid e-mail format" . "\n");
+						}
+					}
+					
+					fclose($fp);
+					fclose($fw);
+					
+					if (mysqli_num_rows($result) > 0) {
+						if(!isset($options['dry_run'])){
+							
+							foreach($user as $value){
+								if($connection->query("INSERT INTO Users (name, surname, email) VALUES
 										(
-											'".addslashes(ucfirst(strtolower($data[0])))."',
-											'".addslashes(ucfirst(strtolower($data[1])))."',
-											'".addslashes(strtolower($data[2]))."'
+											'".addslashes($value['name'])."',
+											'".addslashes($value['surname'])."',
+											'".addslashes($value['e-mail address'])."'
 										)
-									");
-									
-									$count ++;
-								} elseif (!filter_var($data[2], FILTER_VALIDATE_EMAIL) === true){
-									fprintf($fw, "\n" . $data[2] . " is in invalid e-mail format" . "\n");
+									") == TRUE){
+								
+									$rows ++;
+								}else{
+									echo("\n" . "Data Insertion Failed: " . $connection->error . "\n");
 								}
 							}
 							
-							echo "\n" . $count . " row(s) have been succesfully inserted to database" . "\n";
 						}else{
-							die("\n" . "Users Table does not exist, please refer to --help function on how to create the table. Thanks" . "\n");
+							echo "\n" . "Application is running in dry run mode. There is no data that will be imported to database" . "\n";
 						}
 					}else{
-						echo "\n" . "Application is running in dry run mode" . "\n";
+						echo "\n" . "Users Table does not exist, please refer to --help function on how to create the table. Thanks" . "\n";
 					}
-					fclose($fp);
-					fclose($fw);
+					
 				}else{
 					die("\n" . "File is invalid" . "\n");
 				}
-			}else{
-				die("\n" . "Please specify file (users) that is intended to be imported to database" . "\n");
+				echo "\n" . $rows . " row(s) have been succesfully inserted to database" . "\n";
 			}
 			
 			$connection->close();
